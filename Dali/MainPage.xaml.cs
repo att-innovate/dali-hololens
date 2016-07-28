019b93a23466d489177d31793654cb318ed692ed
Sarah Radzihovsky <sradzi13@stanford.edu>
2016-07-28 11:50:05 -0700

    added alerts but they are still very basic and need more functionality

--- Dali/MainPage.xaml.cs
+++ Dali/MainPage.xaml.cs
@@ -11,12 +11,15 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
+using System;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -30,10 +33,33 @@ namespace Dali
         public MainPage()
         {
             this.InitializeComponent();
+            Message();
             // GetRequest("http://127.0.0.1:8085/mark");
             //PostRequest("http://10.250.3.24:8085/mark/");//"http://127.0.0.1:8085/ping");
         }
 
+        public static void Message()
+        {
+            var dlg = new MessageDialog("Would you like to create a new mark or view a previously created mark?");
+            dlg.Commands.Add(new UICommand("Old", null, "OLD"));
+            dlg.Commands.Add(new UICommand("New", null, "NEW"));
+            var op = dlg.ShowAsync();
+            if ((op.Id).ToString() == "OLD")
+            {
+                //change this to give a popup of all the existing marks so user can select one and configure that mark in the current Dali app instance
+                pickOldMark();
+                System.Diagnostics.Debug.WriteLine("MessageDialog Success");
+            }
+        }
+
+        private static void pickOldMark()
+        {
+            var dlg = new MessageDialog("Which mark would you like to view here?");
+            // for loop through marks and add commands for each one so that user can select one
+            //dlg.Commands.Add(new UICommand("Old", null, "OLD"));
+            //dlg.Commands.Add(new UICommand("New", null, "NEW"));
+            //var op = dlg.ShowAsync();
+        }
         async static void GetRequest(string url)
         {
             using (HttpClient client = new HttpClient())
@@ -68,7 +94,7 @@ namespace Dali
                                 .Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
                     HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "relativeAddress");
                     var tempString = "{\"label\":\"#replace#\"}";
-                    var result = tempString.Replace("#replace#" , newLabel);
+                    var result = tempString.Replace("#replace#", newLabel);
                     System.Diagnostics.Debug.Write(result);
                     request.Content = new StringContent(result,
                                                         Encoding.UTF8,
@@ -87,6 +113,19 @@ namespace Dali
             }
         }
 
+
+        /*    private void alert()
+            {
+                try
+                {
+                    // do stuff
+                }
+                catch (Exception ex)
+                {
+                    this.ShowAlert("Alert");
+                }
+            }*/
+
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             /*FILL*/
@@ -96,7 +135,7 @@ namespace Dali
 
         private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
